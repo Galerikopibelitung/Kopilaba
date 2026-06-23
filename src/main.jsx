43d69edf8e8bda34ba8bc028bcb6679c12b56990
Galerikopibelitung <nexvoid.x3@gmail.com>
2026-06-23 22:51:11 +0700

    Initialize main.jsx with React and render KopiLaba

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import KopiLaba from './KopiLaba.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <KopiLaba />
+  </React.StrictMode>
+);
